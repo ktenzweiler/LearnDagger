4463b1a373a737c70d3b7ef569b1d28cbcc1c31e
ktenzweiler <ktenzweiler@gmail.com>
2022-01-15 23:16:41 -0600

    injected field into main activity

--- app/src/main/java/com/example/learndagger/MainActivity.kt
+++ app/src/main/java/com/example/learndagger/MainActivity.kt
@@ -2,18 +2,19 @@ package com.example.learndagger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var car : Car
+    @Inject
+    lateinit var car: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         val component = DaggerCarComponent.create()
+        component.inject(this)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        car = component.getCar()
 
         car.drive()
     }
